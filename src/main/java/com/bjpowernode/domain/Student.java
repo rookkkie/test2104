eee4ca15a4fe809be69dc881f019ebac56fa11b0
zs <zs@bjpowernode.com>
2021-04-13 22:19:14 +0800

    新手的修改

--- src/main/java/com/bjpowernode/domain/Student.java
+++ src/main/java/com/bjpowernode/domain/Student.java
@@ -1,45 +1,17 @@
 package com.bjpowernode.domain;
 
+import lombok.Data;
+
 /**
  * Author 北京动力节点
  */
+@Data
 public class Student
 {
 
     private String id;
     private String name;
     private int age;
-    private int b;
-    public String getId() {
-        return id;
-    }
+    private String phone;
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
