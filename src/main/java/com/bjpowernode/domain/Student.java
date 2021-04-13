32ee38f521ad442eced2305710baccde95f2d5c2
zs <zs@bjpowernode.com>
2021-04-13 22:15:58 +0800

    使用lombok

--- src/main/java/com/bjpowernode/domain/Student.java
+++ src/main/java/com/bjpowernode/domain/Student.java
@@ -1,45 +1,21 @@
 package com.bjpowernode.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Author 北京动力节点
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student
 {
 
     private String id;
     private String name;
     private int age;
-    private int b;
-    public String getId() {
-        return id;
-    }
+    private String address;
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
